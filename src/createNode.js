bba31d8fe5ad359ac16113d7408a26b9d5519290
JaehoLee <spectrick@gmail.com>
2019-02-27 01:33:58 +0900

    fix: aegir linting

--- src/createNode.js
+++ src/createNode.js
@@ -1,31 +1,31 @@
-'use strict';
+'use strict'
 
-const libp2p=require("libp2p");
-const PeerInfo=require("peer-info");
-const WSStar=require("libp2p-websocket-star");
-const Mplex=require("libp2p-mplex");
+const libp2p = require('libp2p')
+const PeerInfo = require('peer-info')
+const WSStar = require('libp2p-websocket-star')
+const Mplex = require('libp2p-mplex')
 
 class Node extends libp2p {
-  constructor(_options) {
-    const wsStar = new WSStar({ id: _options.peerInfo.id });
+  constructor (_options) {
+    const wsStar = new WSStar({ id: _options.peerInfo.id })
     const defaults = {
       modules: {
         transport: [wsStar],
         streamMuxer: [Mplex],
         peerDiscovery: [wsStar.discovery]
       }
-    };
-    super({ ...defaults, ..._options });
+    }
+    super({ ...defaults, ..._options })
   }
 }
 
 const createNode = async websocketStars => new Promise((resolve, reject) => {
   PeerInfo.create((err, peerInfo) => {
-    if (err) reject(err);
-    websocketStars.forEach(addr => peerInfo.multiaddrs.add(addr));
-    const node = new Node({ peerInfo });
-    resolve(node);
-  });
-});
+    if (err) reject(err)
+    websocketStars.forEach(addr => peerInfo.multiaddrs.add(addr))
+    const node = new Node({ peerInfo })
+    resolve(node)
+  })
+})
 
-module.exports = createNode;
+module.exports = createNode
